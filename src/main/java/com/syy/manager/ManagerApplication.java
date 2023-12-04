f2b69140e21b97beb2945d0479e4db8421d0889a
song <930221395@qq.com>
2023-12-04 12:56:55 +0800

    222

--- src/main/java/com/syy/manager/ManagerApplication.java
+++ src/main/java/com/syy/manager/ManagerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = "com.syy.manager")
 public class ManagerApplication {
     public static void main(String[] args) {
+        System.out.println("111");
         SpringApplication.run(ManagerApplication.class, args);
     }
 
